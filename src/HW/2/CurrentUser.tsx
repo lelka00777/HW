a0de2b9754987ed669f6d182229ce9f7431575c1
lelka00777 <lelka00777@gmail.com>
2023-08-08 11:38:19 +0300

    hw2 commit

--- src/HW/2/CurrentUser.tsx
+++ src/HW/2/CurrentUser.tsx
@@ -1,10 +1,12 @@
 import { UserType } from './HW2';
 
 type CurrentUserPropsType = {
-  user: any // пропиши типизацию
+  user: UserType // пропиши типизацию
 };
 
+
 export const CurrentUser = (props: CurrentUserPropsType) => {
+  console.log(props.user)
   return (
     <li key={props.user.id} id={`hw02-user-${props.user.id}`}>
       <strong>{props.user.name}</strong> (Age: {props.user.age})<strong> Address: </strong>
@@ -12,3 +14,5 @@ export const CurrentUser = (props: CurrentUserPropsType) => {
     </li>
   );
 };
+
+
